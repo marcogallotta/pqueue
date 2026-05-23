8a56fb09fb6f4bf8b9f7157251227ea05431bdd9
Marco Gallotta <marco@gallotta.co.za>
2026-05-23 12:33:52 +0200

    fix explicit include missing

--- src/pqueue.h
+++ src/pqueue.h
@@ -6,6 +6,7 @@
 #include "pqueue/store_types.h"
 #include "pqueue/file_system.h"
 #include "pqueue/outbox.h"
+#include "pqueue/http/outbox.h"
 #include "pqueue/queue.h"
 #include "pqueue/status.h"
 #include "pqueue/types.h"
